0d1f26168c0a33e0b298e02f867c38b73ac0ae0a
svyatoslav-reshetnikov <svyatoslav.reshetnikov@gmail.com>
2016-07-11 11:34:33 +0300

    fix: warning in ViewIndicator

--- ReactiveApp/ViewIndicator.swift
+++ ReactiveApp/ViewIndicator.swift
@@ -7,10 +7,11 @@
 //  Inspired by Krunoslav Zaher
 //
 
+import Foundation
 import RxSwift
 import RxCocoa
 
-struct ViewToken<E> : ObservableConvertibleType, Disposable {
+private struct ViewToken<E> : ObservableConvertibleType, Disposable {
     private let _source: Observable<E>
     private let _dispose: AnonymousDisposable
     
@@ -30,33 +31,34 @@ struct ViewToken<E> : ObservableConvertibleType, Disposable {
 
 /**
  Enables monitoring of sequence computation.
- 
  If there is at least one sequence computation in progress, `true` will be sent.
  When all activities complete `false` will be sent.
  */
-class ViewIndicator : DriverConvertibleType {
-    typealias E = Bool
+public class ViewIndicator : DriverConvertibleType {
+    public typealias E = Bool
     
     private let _lock = NSRecursiveLock()
     private let _variable = Variable(0)
     private let _loading: Driver<Bool>
     
-    init() {
+    public init() {
         _loading = _variable.asObservable()
             .map { $0 > 0 }
             .distinctUntilChanged()
-            .asDriver { (error: ErrorType) -> Driver<Bool> in
-                _ = fatalError("Loader can't fail")
-                return Driver.empty()
-            }
+            .asDriver(onErrorRecover: ViewIndicator.ifItStillErrors)
     }
     
-    func trackView<O: ObservableConvertibleType>(source: O) -> Observable<O.E> {
+    private static func ifItStillErrors(error: ErrorType) -> Driver<Bool> {
+        _ = fatalError("Loader can't fail")
+    }
+    
+    
+    private func trackViewOfObservable<O: ObservableConvertibleType>(source: O) -> Observable<O.E> {
         return Observable.using({ () -> ViewToken<O.E> in
             self.increment()
             return ViewToken(source: source.asObservable(), disposeAction: self.decrement)
-            }) { t in
-                return t.asObservable()
+        }) { t in
+            return t.asObservable()
         }
     }
     
@@ -72,13 +74,13 @@ class ViewIndicator : DriverConvertibleType {
         _lock.unlock()
     }
     
-    func asDriver() -> Driver<E> {
+    public func asDriver() -> Driver<E> {
         return _loading
     }
 }
 
-extension ObservableConvertibleType {
-    func trackView(viewIndicator: ViewIndicator) -> Observable<E> {
-        return viewIndicator.trackView(self)
+public extension ObservableConvertibleType {
+    public func trackView(viewIndicator: ViewIndicator) -> Observable<E> {
+        return viewIndicator.trackViewOfObservable(self)
     }
 }
